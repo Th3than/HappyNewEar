e7d4aa18a9f8f895c2bc2972c0eaedbd36cfe659
Natan <Th3than@gmail.com>
2023-01-03 16:17:00 +0700

    lab11-3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include<fstream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    ifstream soup("score.txt");
+    string text;
+    double N,sum,M = 0,S = 0;
+    int i = 0;
+    while (getline(soup, text))
+    {
+        N = atof(text.c_str());
+        sum += N;
+        S += pow(N,2);
+        i++;
+    }
+    soup.close();
+     M = sum/i;
+    S = sqrt((S/i)-pow(M,2));
+    cout << "Number of data = "<<i;
+    cout << setprecision(3);
+    cout << "\nMean = "<<M;
+    cout << "\nStandard deviation = "<<S;
+}
